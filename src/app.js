6fcb3976ce0566bf9a340204e148e942e4faa7da
Alex Schworer <alex.schworer@gmail.com>
2015-01-23 22:40:36 -0500

    use cors proxy hackery

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 var jquery = require('jquery');
 var jservice = require('./jservice');
+
 console.log('neato, Trebek!');
-console.log(jservice.categories());
+
+jservice.categories();
+
--- src/jservice.js
+++ src/jservice.js
@@ -1,12 +1,14 @@
 var jquery = require('jquery');
 
 module.exports.randomQuestion = function() {
-  var response = jquery.get('http://jservice.io/api/random');
-  return response;
+  var response = jquery.get('https://jsonp.nodejitsu.com/?url=http://jservice.io/api/random', function(data) {
+    console.log(data);
+  });
 };
 
 module.exports.categories = function(count) {
-  var response = jquery.get('http://jservice.io/api/categories');
-  return response;
+  var response = jquery.get('https://jsonp.nodejitsu.com/?url=http://jservice.io/api/categories', function(data){
+    console.log(data);
+  });
 };
 
