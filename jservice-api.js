574f12ca1f794d12dff71461bede7f476f7ef659
Alex Schworer <alex.schworer@gmail.com>
2015-01-23 21:10:40 -0500

    Add categories call

--- jservice-api.js
+++ jservice-api.js
@@ -1,4 +1,11 @@
+
 var randomQuestion = function() {
   var response = $.get('http://jservice.io/api/random');
   return response;
 };
+
+var categories = function(count) {
+  var response = $.get('http://jservice.io/api/categories');
+  return response;
+}
+
