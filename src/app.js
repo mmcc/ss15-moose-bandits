6e92d4c6ecebe84b9911ad7eff91741b312666b3
Matt McClure <matt@htbx.net>
2015-01-25 00:10:17 -0800

    join a current game via query params

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 var jquery = require('jquery');
 var jservice = require('./jservice');
 var answers = require('./answers');
-var Jeopardy = require('./jeopardy')
-jeopardy = new Jeopardy("https://moose-bandits.firebaseio.com/")
+var utils = require('./utils');
+var Jeopardy = require('./jeopardy');
+jeopardy = new Jeopardy("https://moose-bandits.firebaseio.com/");
 
-//Creates a new game every refresh right now
-jeopardy.createGame(function(err, gameId){
+// Check for the current game in the query params
+var currentGame = utils.getQueryParams()['current-game'];
+
+// If there is a current game, use that when init-ing.
+// TODO: Replace this with a check for whether or not this should just be a client
+// joining a game. Perhaps another query string specifying whether or not this is
+// a display?
+returnOrCreateGame(currentGame, function(err, gameId) {
   //Start up a new server
   jeopardy.initServer(gameId, function(err, server){
-    if(err) { console.log(err); return}
+    if (err) return console.error(err);
     console.log(server.gameId)
 
     //This callback happens anytime the displayable "board" changes
@@ -19,9 +26,18 @@ jeopardy.createGame(function(err, gameId){
   });
 
   jeopardy.initClient(gameId, function(err, client) {
-    if(err) {console.log(err); return;}
+    if (err) return console.error(err);
+
     client.loginUser("doot", function() {
 
     });
   });
 });
+
+// TODO: This should probably go in one of the jeopardy classes.
+// Just a simple function to allow us to avoid duplicating code. Expects a callback(err, gameId)
+function returnOrCreateGame(id, cb) {
+  if (id) return cb(null, id);
+
+  jeopardy.createGame(cb);
+}
