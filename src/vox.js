59245b8e79d378bb5e6db71d5430e24866924d06
Alex Schworer <alex.schworer@gmail.com>
2015-01-25 12:34:48 -0500

    cleanup

--- src/vox.js
+++ src/vox.js
@@ -1,9 +1,7 @@
 module.exports.say = function(text, onEnd) {
   var utterance = new SpeechSynthesisUtterance(text);
   utterance.addEventListener('end', onEnd);
-  setTimeout(function() {
-    window.speechSynthesis.speak(utterance)
-  }, 100);
+  window.speechSynthesis.speak(utterance)
 };
 
 module.exports.tell = function(callback) {
@@ -11,6 +9,7 @@ module.exports.tell = function(callback) {
   recognition.continuous = false;
   recognition.onresult = function(event) {
     if (event.results.length > 0) {
+      // send the text that was recognized to the callback
       callback(event.results[0][0].transcript);
     }
   }
